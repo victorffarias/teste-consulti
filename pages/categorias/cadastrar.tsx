506418f4dbc17a590cd56e096b9a5b621741726e
Victor Farias <victor.f.farias@gmail.com>
2021-09-28 21:16:50 -0300

    [ADD] Criação da listagem de categorias

--- pages/categorias/cadastrar.tsx
+++ pages/categorias/cadastrar.tsx
@@ -7,6 +7,7 @@ export default function Cadastrar(){
     return (
         <div className={styles.pageWrapper}>
             <main>
+                <h1>Cadastro de Categoria</h1>
                 <FormCategoria />
             </main>
         </div>        
--- pages/categorias/index.tsx
+++ pages/categorias/index.tsx
@@ -1,5 +1,124 @@
-export default function Index() {
+import { GetStaticProps } from "next";
+import { useRouter } from "next/dist/client/router";
+import { api } from "../../services/api";
+import styles from "../../styles/categorias/index.module.scss";
+
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import Categoria from "./[slug]";
+import Button from "../../components/Button";
+
+
+interface Categoria {
+    Descricao: string;
+    Nome: string;
+    Ordem: number;
+    Email: string;
+    ID: number
+}
+
+interface IndexProps {
+    categorias: Categoria[]
+}
+
+export default function Index({ categorias }: IndexProps) {
+    const router = useRouter()
+    function handleEdit(categoriaId: number) {
+        router.push(`categorias/${categoriaId}`)
+    }
+
+    function handleCadastrar() {
+        console.log("teste");
+        router.push("categorias/cadastrar");
+    }
+
+    function handleConfirmDelete(categoriaId: number) {
+        confirmAlert({
+            title: "Atenção",
+            message: "Confirmma a exclusão do objeto?",
+            buttons: [
+                {
+                    label: "Sim",
+                    onClick: () => handleDelete(categoriaId)
+                },
+                {
+                    label: "Não",
+                    onClick: () => void (0)
+                }
+            ],
+        })
+    }
+
+    async function handleDelete(categoriaId: number) {
+        await api.delete(
+            "/api/excluir-categoria",
+            { params: { id: categoriaId } }
+        );
+
+        confirmAlert({
+            title: "Sucesso",
+            message: "Categoria excluída com sucesso",
+            buttons: [
+                {
+                    label: "Fechar",
+                    onClick: () => void (0)
+                }
+            ],
+        })
+
+        router.replace(router.asPath);
+    }
+
     return (
-        <h1>Listagem de categorias</h1>
+        <div className={styles.pageWrapper}>
+            <main>
+                <h1>Categorias</h1>
+                <div className={styles.buttonsWrapper}>
+                    <Button onClick={handleCadastrar}>Cadastrar</Button>
+                </div>
+                <table cellSpacing={0} >
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Descrição</th>
+                            <th>Ordem</th>
+                            <th>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {categorias.map((categoria, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{categoria.Nome}</td>
+                                    <td>{categoria.Descricao}</td>
+                                    <td align="right">{categoria.Ordem}</td>
+                                    <td align="center">
+                                        <a href="#" onClick={() => { handleEdit(categoria.ID) }}>
+                                            <img src="/edit.png" alt="Editar Categoria" />
+                                        </a>
+
+                                        <a href="#" onClick={() => { handleConfirmDelete(categoria.ID) }}>
+                                            <img src="/delete.png" alt="Apagar Categoria" />
+                                        </a>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+
+                    </tbody>
+                </table>
+            </main>
+        </div>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const { data } = await api.get("/api/listar-categorias-por-email")
+
+    return {
+        props: {
+            categorias: data.ObterCategoriasPorEmailResult
+        }
+    }
 }
