f66c250b2c33eb27bdf571c22bff6a7f6abb7481
Victor Farias <victor.f.farias@gmail.com>
2021-09-28 21:14:58 -0300

    [ADD] Criação da pagina de edição da categoria

--- pages/api/cadastrar-categoria.ts
+++ pages/api/cadastrar-categoria.ts
@@ -1,23 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import NextCors from 'nextjs-cors';
 import { service } from '../../services/api';
+import usePreflight from './usePreflight';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        await NextCors(req, res, {
-            methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-            origin: '*',
-            optionsSuccessStatus: 200
-        });
-
-        res.json({ message: 'CORS Preflight' });
+        await usePreflight(req, res);
 
         const result = await service.post(
             "CategoriaService.svc/CadastrarCategoria",  
             req.body.param
         );
 
-        return result;
+        res.end(JSON.stringify(result.data));
     } catch (error: any) {
         console.error(error)
         return res.status(error.status || 500).end(error.message)
